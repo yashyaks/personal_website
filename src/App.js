2004a640c4f79e6f147b6c029488d0a43b9bf937
Yash Thakar <90375840+yashyaks@users.noreply.github.com>
2023-03-01 09:24:02 +0530

    Nav Bar done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,43 @@ function App() {
   return (
     <div>
       <Navbar/>
+      <div class="content">
+      
+      
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante lacus, pretium eu eleifend a, porttitor nec enim. Aliquam erat volutpat. Morbi feugiat venenatis felis, pulvinar aliquet arcu facilisis eget. Duis pellentesque finibus dapibus. Morbi vel nisl metus. Nam fringilla lorem vitae justo ullamcorper, eu volutpat orci pellentesque. Proin vitae lacus quis massa mattis congue.
+
+Aenean posuere justo magna, nec vulputate mauris ultrices id. Aenean ligula sapien, hendrerit imperdiet dictum ut, condimentum in neque. Aenean hendrerit felis felis, quis eleifend dui euismod sit amet. Phasellus eget orci dignissim, blandit neque a, ultrices mauris. Nunc ac mauris libero. Duis fermentum risus at risus elementum dignissim. Nulla facilisi. Cras lobortis leo est, sit amet mollis risus tincidunt at. Duis rhoncus turpis in placerat elementum. In nec lobortis dui. Vestibulum tristique ultrices tellus eget fermentum.
+
+Nunc egestas velit arcu, sed consectetur tortor tempus eget. Vestibulum sit amet ante iaculis, blandit dui nec, accumsan velit. Proin iaculis ultrices mauris, ut fringilla sem vulputate in. Integer aliquam laoreet felis, ut accumsan dui ultrices eu. Suspendisse auctor auctor augue, nec sagittis tortor tincidunt sed. Sed eros ex, sollicitudin vitae sagittis a, rutrum ac nunc. Phasellus molestie quam eu lacus ullamcorper, ut viverra justo condimentum. Cras congue lacinia nunc, pretium facilisis turpis rhoncus quis. Pellentesque magna turpis, pellentesque consectetur rutrum at, scelerisque sed metus. Etiam et sollicitudin mi.
+
+Sed faucibus id ligula sed tristique. Aenean justo urna, rutrum at mi nec, luctus pharetra augue. Proin lacinia mauris ipsum, et porttitor sem pellentesque et. Cras cursus, nibh sed viverra cursus, purus mauris tristique lacus, vitae semper mi neque sed neque. Curabitur accumsan urna sed sem commodo, eu eleifend ligula placerat. Etiam commodo pellentesque purus. Morbi laoreet nibh lobortis, porta orci facilisis, sagittis lectus. Cras at nisl nisl.
+
+Fusce fermentum mi ut varius eleifend. Praesent at erat lorem. Donec lobortis dolor at massa suscipit dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus consectetur, odio egestas fermentum placerat, sem massa cursus nulla, eget volutpat nisl tortor ut odio. In bibendum risus libero, eu lobortis nibh molestie vitae. Donec euismod quam quis quam vestibulum, vel ultricies dolor aliquam. Donec pulvinar ante aliquet lorem ultricies laoreet.
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante lacus, pretium eu eleifend a, porttitor nec enim. Aliquam erat volutpat. Morbi feugiat venenatis felis, pulvinar aliquet arcu facilisis eget. Duis pellentesque finibus dapibus. Morbi vel nisl metus. Nam fringilla lorem vitae justo ullamcorper, eu volutpat orci pellentesque. Proin vitae lacus quis massa mattis congue.
+
+Aenean posuere justo magna, nec vulputate mauris ultrices id. Aenean ligula sapien, hendrerit imperdiet dictum ut, condimentum in neque. Aenean hendrerit felis felis, quis eleifend dui euismod sit amet. Phasellus eget orci dignissim, blandit neque a, ultrices mauris. Nunc ac mauris libero. Duis fermentum risus at risus elementum dignissim. Nulla facilisi. Cras lobortis leo est, sit amet mollis risus tincidunt at. Duis rhoncus turpis in placerat elementum. In nec lobortis dui. Vestibulum tristique ultrices tellus eget fermentum.
+
+Nunc egestas velit arcu, sed consectetur tortor tempus eget. Vestibulum sit amet ante iaculis, blandit dui nec, accumsan velit. Proin iaculis ultrices mauris, ut fringilla sem vulputate in. Integer aliquam laoreet felis, ut accumsan dui ultrices eu. Suspendisse auctor auctor augue, nec sagittis tortor tincidunt sed. Sed eros ex, sollicitudin vitae sagittis a, rutrum ac nunc. Phasellus molestie quam eu lacus ullamcorper, ut viverra justo condimentum. Cras congue lacinia nunc, pretium facilisis turpis rhoncus quis. Pellentesque magna turpis, pellentesque consectetur rutrum at, scelerisque sed metus. Etiam et sollicitudin mi.
+
+Sed faucibus id ligula sed tristique. Aenean justo urna, rutrum at mi nec, luctus pharetra augue. Proin lacinia mauris ipsum, et porttitor sem pellentesque et. Cras cursus, nibh sed viverra cursus, purus mauris tristique lacus, vitae semper mi neque sed neque. Curabitur accumsan urna sed sem commodo, eu eleifend ligula placerat. Etiam commodo pellentesque purus. Morbi laoreet nibh lobortis, porta orci facilisis, sagittis lectus. Cras at nisl nisl.
+
+Fusce fermentum mi ut varius eleifend. Praesent at erat lorem. Donec lobortis dolor at massa suscipit dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus consectetur, odio egestas fermentum placerat, sem massa cursus nulla, eget volutpat nisl tortor ut odio. In bibendum risus libero, eu lobortis nibh molestie vitae. Donec euismod quam quis quam vestibulum, vel ultricies dolor aliquam. Donec pulvinar ante aliquet lorem ultricies laoreet.
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante lacus, pretium eu eleifend a, porttitor nec enim. Aliquam erat volutpat. Morbi feugiat venenatis felis, pulvinar aliquet arcu facilisis eget. Duis pellentesque finibus dapibus. Morbi vel nisl metus. Nam fringilla lorem vitae justo ullamcorper, eu volutpat orci pellentesque. Proin vitae lacus quis massa mattis congue.
+
+Aenean posuere justo magna, nec vulputate mauris ultrices id. Aenean ligula sapien, hendrerit imperdiet dictum ut, condimentum in neque. Aenean hendrerit felis felis, quis eleifend dui euismod sit amet. Phasellus eget orci dignissim, blandit neque a, ultrices mauris. Nunc ac mauris libero. Duis fermentum risus at risus elementum dignissim. Nulla facilisi. Cras lobortis leo est, sit amet mollis risus tincidunt at. Duis rhoncus turpis in placerat elementum. In nec lobortis dui. Vestibulum tristique ultrices tellus eget fermentum.
+
+Nunc egestas velit arcu, sed consectetur tortor tempus eget. Vestibulum sit amet ante iaculis, blandit dui nec, accumsan velit. Proin iaculis ultrices mauris, ut fringilla sem vulputate in. Integer aliquam laoreet felis, ut accumsan dui ultrices eu. Suspendisse auctor auctor augue, nec sagittis tortor tincidunt sed. Sed eros ex, sollicitudin vitae sagittis a, rutrum ac nunc. Phasellus molestie quam eu lacus ullamcorper, ut viverra justo condimentum. Cras congue lacinia nunc, pretium facilisis turpis rhoncus quis. Pellentesque magna turpis, pellentesque consectetur rutrum at, scelerisque sed metus. Etiam et sollicitudin mi.
+
+Sed faucibus id ligula sed tristique. Aenean justo urna, rutrum at mi nec, luctus pharetra augue. Proin lacinia mauris ipsum, et porttitor sem pellentesque et. Cras cursus, nibh sed viverra cursus, purus mauris tristique lacus, vitae semper mi neque sed neque. Curabitur accumsan urna sed sem commodo, eu eleifend ligula placerat. Etiam commodo pellentesque purus. Morbi laoreet nibh lobortis, porta orci facilisis, sagittis lectus. Cras at nisl nisl.
+
+Fusce fermentum mi ut varius eleifend. Praesent at erat lorem. Donec lobortis dolor at massa suscipit dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus consectetur, odio egestas fermentum placerat, sem massa cursus nulla, eget volutpat nisl tortor ut odio. In bibendum risus libero, eu lobortis nibh molestie vitae. Donec euismod quam quis quam vestibulum, vel ultricies dolor aliquam. Donec pulvinar ante aliquet lorem ultricies laoreet.
+
+ 
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import NavBar from 'react-bootstrap/Navbar';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import Logo from "../Navbar/favicon.png"
 import './Navbar.css'
 
 export default function Navigation() {
   return (
     <div className='navBar'>
-      <NavBar bg="myYellow" variant='light'>
-        <NavBar.Brand>
+      <Navbar bg="myYellow" variant='light' fixed="top" expand="lg" collapseOnSelect>
+        <Navbar.Brand>
           <img  src={Logo} alt="yashyaks"/>
           <span className='name'>Yash Thakar</span>
-        </NavBar.Brand>
-      </NavBar>
+        </Navbar.Brand>
+        <Navbar.Toggle/>
+        <Navbar.Collapse className='right-aligned'>
+          <Nav>
+            <Nav.Link href="#">Projects</Nav.Link>
+            <Nav.Link href="#">Experience</Nav.Link>
+            <Nav.Link href="#">Skills</Nav.Link>
+            <Nav.Link href="#">Education</Nav.Link>
+            <Nav.Link href="#">Achievements</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="https://yashyaks.hashnode.dev/" target = "_blank">Blog</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+      
+      </Navbar>
     </div>
 
   )
